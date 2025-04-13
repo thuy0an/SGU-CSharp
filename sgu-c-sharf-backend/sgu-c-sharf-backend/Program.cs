65ff27e51325b777f4a43e95dc50e7ae9ba22c62
manas1124 <khanhday1124@gmail.com>
2025-04-13 09:28:01 +0700

    addscope phieumuon vao program

--- sgu-c-sharf-backend/sgu-c-sharf-backend/Program.cs
+++ sgu-c-sharf-backend/sgu-c-sharf-backend/Program.cs
@@ -17,6 +17,9 @@ builder.Services.AddScoped<LoaiThietBiService>();
 builder.Services.AddScoped<PhieuDatChoRepository>();
 builder.Services.AddScoped<PhieuDatChoService>();
 
+builder.Services.AddScoped<PhieuMuonRepository>();
+builder.Services.AddScoped<PhieuMuonService>();
+
 // Đăng ký MySqlConnection vào DI Container
 builder.Services.AddTransient<MySqlConnection>(_ =>
     new MySqlConnection(builder.Configuration.GetConnectionString("DefaultConnection"))
